db459dcf96435248bc02378d224d3864d3eb6b46
Jonas Larsson <jonas_larsson1@outlook.com>
2024-11-06 09:54:01 +0100

    init commit

---
+++ JavaSpringExam/src/main/java/com/Group3/JavaSpringExam/JavaSpringExamApplication.java
@@ -0,0 +1,13 @@
+package com.Group3.JavaSpringExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Group3/JavaSpringExam/JavaSpringExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group3.JavaSpringExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
