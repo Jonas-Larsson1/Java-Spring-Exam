bf69334f5b695582d19dd0549ca14ee9cb7db05d
Jonas Larsson <jonas_larsson1@outlook.com>
2024-11-09 08:46:39 +0100

    create loancontroller

---
+++ src/main/java/com/Group3/JavaSpringExam/Loan/LoanController.java
@@ -0,0 +1,26 @@
+package com.Group3.JavaSpringExam.Loan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/loan")
+public class LoanController {
+
+  private final LoanService loanService;
+
+  @Autowired
+  public LoanController(LoanService loanService) {
+    this.loanService = loanService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Loan> createLoan(@RequestBody Loan loan) {
+    return loanService.addLoan(loan);
+  }
+
+}
